a75665833d1f3c9bf7d2e43ea14040f44ecfde18
Darakhshan Juniad <156326631+Darakhshan2@users.noreply.github.com>
2024-03-05 09:39:33 +0500

    Add files via upload
    IN THIS REPOSITORY I ALSO COMPLETED CLASS ASSIGNMENT WHICH IS GIVEN FOR PRACTICE PURPOSE

---
+++ class_assignment.js
@@ -0,0 +1,77 @@
+"use strict";
+// HOMEWORK ASSIGNMENT
+//  " ````` COMPARISON OPERATORS`````` "
+let a = 5;
+let b = 4;
+let equal = a == b; // no type checking 
+console.log(equal); // false
+let c = 4;
+let d = "4";
+let equal2 = c === d; // equal value & equal type
+console.log(equal2); // false
+let e = 9;
+let f = "9";
+let equal1 = (f == e); // equal value only
+console.log(equal1); // true
+let j = 10;
+let i = 10;
+let cal = j != i; // not equal value
+console.log(cal); // false
+let k = 10;
+let l = "10";
+let calculate = k !== l; // equal value equal type
+console.log(calculate); // true
+let m = 5;
+let o = 4;
+let greaterthan = m > o; // m is greaterthan o
+console.log(greaterthan); // true
+let p = 3;
+let q = 2;
+let lessthan = q < p; // q is lessthan p
+console.log(lessthan); // true
+let r = 8;
+let s = 7;
+let greather_equal = r >= s; // r is greaterthan / equal to
+console.log(greather_equal); // true
+let t = 6;
+let u = 5;
+let equal_to = t <= u; // t is lessthan / equal to
+console.log(equal_to); //false
+// --------X-------------X-----------------X-------
+//````` "VARIABLE UPDATION"````````
+let A = 3;
+A += 4; // ALSO WRITTEN AS (A=A+4)=>(A=3+4)
+console.log(A); // 7
+let B = 6;
+B -= 2; // 6-2
+console.log(B); // 4
+// "```````````ARTHEMATHIC OPERATION``````````"
+let S = 4;
+let L = 6;
+let SUM = S + L;
+console.log(SUM); // 10
+let R = 10;
+let H = 5;
+let DIFF = R - H;
+console.log(DIFF); // 5
+let D = 9;
+let F = 6;
+let MULTI = D * F;
+console.log(MULTI); // 54
+let Q = 81;
+let W = 9;
+let DIVIDE = Q / W; // gives answer
+console.log(DIVIDE); // 9
+let Y = 50;
+let U = 3;
+let MODULUS = Y % U; // gives reminder
+console.log(MODULUS); // 2 
+let Z = 6;
+let C = 4;
+let POWER = Z ** C; // 6 KI POWER 4 (6*6*6*6)
+console.log(POWER); // 1296
+// ```````CONCATINATION``````````
+let myName = "DARAKHSHAN";
+let last_Name = "JUNIAD";
+let CONCATE = myName.concat(" ", last_Name); // myName + last_Name 
+console.log(CONCATE);
---
+++ operators.js
@@ -0,0 +1,67 @@
+"use strict";
+//" OPERATORS IN TYPESCRIPT"
+// operators allow us to perform operation on varibles examples (+,-,*,/,%)
+//  "SUM"
+// console.log(5+3) --> ex no 1 
+// ADDITION "+"  --> ex no 2
+const num1 = 10;
+const num2 = 6;
+const sum = num1 + num2 + 10;
+console.log(sum);
+// SUBSTRACTION "-" --> EX NO 1
+const num = 10;
+const num3 = 2;
+const diff = num - num3;
+console.log(diff);
+// Multiplication "*"
+const num4 = 3;
+const num6 = 2;
+const mul = num4 * num6;
+console.log(mul);
+console.log(2 >= 5);
+// DIVISION "/"
+const num7 = 9;
+const num8 = 3;
+const div = num7 / num8;
+console.log(div);
+// MODULUS "%"
+const num0 = 21;
+const num9 = 5;
+const modulo = num0 % num9;
+console.log(modulo);
+// POWER "**"
+const numi = 5;
+const numk = 2;
+const power = numi ** numk;
+//            5    **    2
+console.log(power);
+// concatination 
+const name1 = "darakhshan";
+const name2 = "juniad";
+console.log(name1 + " " + name2); // " " --> used for spacing
+const firstName = "darakhshan";
+const lastName = 8;
+console.log(firstName + lastName); // STRING + NUM TO CONCATENATION HOTI HAI HAI " USKI TYPE STRING HI HOTI HAI"
+// variable updation
+let n = 5;
+//n = n + 5  --> normal methor  
+n += 5; // -->shorthand
+console.log(n);
+// COMPARISON OPERATORS
+// comparison => operators used to compare
+// compariosn is in the from of boolean (True/ false)
+// is equal to --> == no type checking
+// === --> also type chceking
+// not equal --> !=
+// greater than  --> ">" 
+// less than  -->  "<"
+console.log(5 == 5); // equal to
+// console.log(5 == "5")  // sirf value check hogi type nh
+//console.group(5 === "2")   //is mn type bh check hogi sirf value ko nh dekhajaye ga
+//console.log(10 != 5)
+console.log(5 > 2); //greaterthan
+console.log(5 < 2); // lessthan
+console.log(5 <= 5);
+console.log(2 <= 5); // agr 2no mn sy aik bh true hojaye condition to true aye ga
+//CLASS HOMEWORK 
+// TO PRACTICE ALL THE OPERATORES AND STORE IT ON A NEW VARIABLE
